6152fd717d02d8c366ff53043a922c1fc938093d
Malte <skyguardian42@gmail.com>
2020-04-09 19:37:11 +0200

    better form accessibility

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -32,16 +32,16 @@ class ContactForm extends React.Component {
         <form name="contact" onSubmit={this.handleSubmit} method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
           <div className={style.inputWrapper}>
-            <input type="text" name="name"  onChange={this.change} ref={this.name} required/>
-            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="form_name" onChange={this.change} ref={this.name} required/>
+            <label htmlFor="form_name">Name</label>
           </div>
           <div className={style.inputWrapper}>
-            <input type="email" name="email" onChange={this.change} ref={this.email} required/>
-            <label htmlFor="email">Email-Adresse</label>
+            <input type="email" name="email" id="form_email" onChange={this.change} ref={this.email} required/>
+            <label htmlFor="form_email">Email-Adresse</label>
           </div>
           <div className={style.inputWrapper}>
-            <textarea name="message" rows="6" onChange={this.change} ref={this.message} required></textarea>
-            <label htmlFor="message">Nachricht</label>
+            <textarea name="message" rows="6" id="form_message" onChange={this.change} ref={this.message} required></textarea>
+            <label htmlFor="form_message">Nachricht</label>
           </div>
           <div className={style.inputWrapper}>
             <button type="submit" ref={this.input}>Abschicken</button>
