15fd911edab5f30e156591041f82d243fddee30d
Malte <skyguardian42@gmail.com>
2020-04-09 12:14:24 +0200

    real 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,8 @@ import React from "react"
 import Nav from "../components/nav"
 
 export default () => (
-  <div className="app">
-    <Nav/>>
-    <p>404: nicht gefunden</p>
+  <div className="app" style={{"padding-top": "2rem"}}>
+    <Nav/>
+    <p style={{"textAlign": "center", "padding-top": "49vh", "margin": "0"}}>404: nicht gefunden <br/><br/><a href="/">Zurück zur Seite</a></p>
   </div>
 )
