9331696bc292008bd7479e5b08d62711034bb740
Malte <skyguardian42@gmail.com>
2020-04-09 19:54:17 +0200

    og test

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -7,23 +7,23 @@ export default () => {
   const { site } = useStaticQuery(query)
   const { siteMetadata:meta } = site
   return(
-  <Helmet htmlAttributes={{"lang": "de"}}>
+  <Helmet htmlAttributes={{"lang": "de"}} meta={[
+    { name: `og:title`,
+      content: meta.title
+    },
+  ]}>
     <title>{meta.title}</title>
-    <meta property="og:title" content={meta.title} />
+    {/* <meta property="og:title" content={meta.title} /> */}
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content={meta.description} />
     <meta property="og:url" content="https://malts.me" />
+    <meta property="og:locale" content="de_DE" />
+    <meta property="og:image" content={meta.image} />
 
     <meta name="description" content={meta.description} />
-    <meta property="og:description" content={meta.description} />
-    
-    <meta property="og:type" content="website" />
-
     <meta name="language" content="DE"/>
-    <meta property="og:locale" content="de_DE" />
-
-    <meta property="og:image" content={meta.image} />
+    
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital@0;1&display=swap" rel="stylesheet"/>
-
-
   </Helmet>
 )}
 
