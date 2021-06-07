1a48f9603914cb0b99b3544bde9bda1214a736a7
Malte <skyguardian42@gmail.com>
2021-06-07 13:39:05 +0200

    fix seo image

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -19,12 +19,12 @@ export default ({title, description, image}) => {
     <meta property="og:description" content={description} />
     <meta property="og:url" content="https://malts.me" />
     <meta property="og:locale" content="de_DE" />
-    <meta property="og:image" content={image} />
+    <meta property="og:image" content={`https://malts.me${image}`} />
 
     <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:title" content={title}/>
     <meta name="twitter:description" content={description}/>
-    <meta name="twitter:image" content={image}/>
+    <meta name="twitter:image" content={`https://malts.me${image}`}/>
 
     <meta name="description" content={description} />
     <meta name="language" content="DE"/>
