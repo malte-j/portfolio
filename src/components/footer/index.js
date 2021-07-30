dbff354c0f38bc66ffa8a5e6626cb3f6e488d0d9
Malte <skyguardian42@gmail.com>
2021-07-30 16:15:55 +0200

    fix ssr bug

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,25 +1,23 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
 import style from "./footer.module.scss"
 
-//create your forceUpdate hook
-function useForceUpdate(){
-  const [, setValue] = useState(0); // integer state
-  return () => setValue(value => value + 1); // update the state to force render
-}
-
 export default () => {
-  const forceUpdate = useForceUpdate();
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    setTheme(document.body.dataset.theme);
+  }, [])
 
 
   function toggleTheme() {
-    if(document.body.dataset.theme === "dark") {
+    if (document.body.dataset.theme === "dark") {
       document.body.dataset.theme = "light";
       localStorage.setItem("theme", "light")
-      forceUpdate();
+      setTheme("light");
     } else {
       document.body.dataset.theme = "dark";
       localStorage.setItem("theme", "dark")
-      forceUpdate();
+      setTheme("dark");
     }
   }
 
@@ -27,19 +25,13 @@ export default () => {
     <footer className={style.footer}>
       <p>© {new Date().getFullYear()} Malte Janßen</p>
       <a href="https://www.linkedin.com/in/malte-j" rel="noreferrer" target="_blank">LinkedIn</a>
-  
+
       <a href="https://github.com/skyguardian42" rel="noreferrer" target="_blank">GitHub</a>
-  
+
       <a href="mailto:hi@malts.me" rel="noreferrer" target="_blank">hi@malts.me</a>
 
       <button onClick={toggleTheme} className={style.themeToggle}>
-        {
-          document.body.dataset.theme === "dark" ? (
-            <img src="/sun.svg" alt="toggle theme" />
-          ) : (
-            <img src="/moon.svg" alt="toggle theme" />
-          )
-        }
+        <img src={ theme === "dark" ? "/sun.svg" : "/moon.svg" } alt="toggle theme" />
       </button>
     </footer>
   )
