2a1bf070901e8050cc510432502d89ae4b59f65b
Malte <skyguardian42@gmail.com>
2021-07-30 17:34:12 +0200

    use canvas for video slider

--- content/posts/image-frontend/ImageSlider.js
+++ content/posts/image-frontend/ImageSlider.js
@@ -1,28 +1,62 @@
-import React, {useRef} from "react"
+import React, { useRef } from "react"
 
 export default () => {
-  const videoRef = useRef();
+  const videoRef = useRef()
+  const canvasRef = useRef()
 
+  const drawImage = () => {
+    const ctx = canvasRef.current?.getContext("2d")
+    if (
+      videoRef.current === undefined ||
+      canvasRef.current === undefined ||
+      ctx === undefined
+    ) {
+      return
+    }
 
+    ctx.drawImage(
+      videoRef.current,
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    )
+  }
 
-  function setTime(percentage) {
-    if(!videoRef.current) return
+  function handleSliderChange(e) {
+    if (!videoRef.current) return;
+    videoRef.current.currentTime = videoRef.current.duration * (e / 100)
 
-    videoRef.current.currentTime = videoRef.current.duration * (percentage / 100)
+    const step = () => {
+      drawImage()
+    }
+
+    requestAnimationFrame(step)
   }
 
   return (
-    <div style={{"marginBottom": "1.8rem"}}>
-      <video ref={videoRef} style={{"width": "100%", "height": "auto", "aspectRatio": "608/240" }} src="/lqip_loading_blur_up.mp4"/>
-      <input defaultValue={0} style={{width: "100%"}} type="range" step={0.01} onChange={e => setTime(e.target.value)} />
+    <div style={{ marginBottom: "1.8rem" }}>
+      <video
+        autoPlay="true"
+        onPlay={() => {
+          videoRef.current.pause()
+          drawImage()
+        }}
+        ref={videoRef}
+        style={{ width: "100%", height: "auto", aspectRatio: "608/240", position: "absolute", visibility: "hidden" }}
+        src="/lqip_loading_blur_up.mp4"
+      />
+      <canvas
+        style={{ width: "100%", height: "auto", aspectRatio: "608/240" }}
+        ref={canvasRef}
+      />
+      <input
+        defaultValue={0}
+        style={{ width: "100%" }}
+        type="range"
+        step={0.01}
+        onChange={e => handleSliderChange(e.target.value)}
+      />
     </div>
   )
 }
-
-/*
-<!-- <figure>
-  <video style={{"width": "100%", "height": "auto", "aspect-ratio": "608/240" }} src="/lqip_loading_blur_up.mp4" controls/>
-<figcaption>
-</figcaption>
-</figure> -->
-*/
