57401ec8f4daf332924cc69f7aa7609b0f8d3506
Malte <hi@malts.me>
2022-11-18 13:50:59 +0100

    add crossOrigin="anonymous"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,12 +24,14 @@ export default function Index({ data }) {
           href="/fonts/PerfectlyNineties-Regular.woff2"
           as="font"
           type="font/woff2"
+          crossOrigin="anonymous"
         />
         <link
           rel="preload"
           href="/fonts/PerfectlyNineties-Italic.woff2"
           as="font"
           type="font/woff2"
+          crossOrigin="anonymous"
         />
       </Helmet>
 
